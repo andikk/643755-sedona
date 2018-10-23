9c49b2b1e813139040e84cbec0483ea31a2d8e42
andikk <andikk@mail.ru>
2018-10-23 11:49:16 +0300

    правки gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var del = require("del");
+
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
@@ -29,6 +35,60 @@ gulp.task("server", function () {
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+gulp.task("style", function () {
+  gulp.src("source/less/style.scss")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+    imagemin.optipng({
+        optimizationLevel: 3
+      }),
+    imagemin.jpegtran({
+        progressive: true
+      }),
+    imagemin.svgo()
+  ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**",
+    "source/js/**",
+    "source/*.html"
+  ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("build", function (done) {
+  run(
+    "clean",
+    "style",
+    "images",
+    "copy",
+    done
+  );
 });
 
 gulp.task("start", gulp.series("css", "server"));
